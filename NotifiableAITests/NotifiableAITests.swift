72aca58e029f1c456c871dad0a4159ff867a9f43
Matt Brooke-Smith <matt@futureworkshops.com>
2026-05-08 09:13:08 +0100

    Initial Commit

---
+++ NotifiableAITests/NotifiableAITests.swift
@@ -0,0 +1,16 @@
+//
+//  NotifiableAITests.swift
+//  NotifiableAITests
+//
+//  Created by Matt Brooke-Smith on 08/05/2026.
+//
+
+import Testing
+
+struct NotifiableAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
